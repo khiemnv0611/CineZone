6b523b09d05986fed4634405a3acabeb1f9e4c0c
Vĩnh Khiêm Nguyễn <ngvinhkhiem123@gmail.com>
2024-11-12 21:30:15 +0700

    feat: config firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -32,7 +33,7 @@ android {
 }
 
 dependencies {
-
+    implementation(platform(libs.firebase.bom))
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
